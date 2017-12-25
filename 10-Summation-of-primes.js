566d5c027700116b3f6f0d48bdf4f96cc47a1a93
jstudenski <jstudenski@gmail.com>
2017-12-25 00:12:20 -0600

    10 progress

--- 10-Summation-of-primes.js
+++ 10-Summation-of-primes.js
@@ -1,3 +1,26 @@
 // The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 // Find the sum of all the primes below two million.
+
+
+
+var sum = 2;
+var prime = 3;
+
+while (prime < 2000000) {
+  if (isPrime(prime)) {
+    console.log(prime);
+    sum += prime
+  }
+
+  prime = prime+2;
+}
+
+console.log(sum);
+
+function isPrime(num) {
+  for(var i = 2; i < num; i++)
+    if(num % i === 0) return false;
+  return num !== 1;
+}
+
