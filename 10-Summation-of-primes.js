a55d03fdeaf3e80d49d6709f86928aa55ec51f09
jstudenski <jstudenski@gmail.com>
2017-12-26 11:11:28 -0600

    10 solverd

--- 10-Summation-of-primes.js
+++ 10-Summation-of-primes.js
@@ -2,24 +2,20 @@
 
 // Find the sum of all the primes below two million.
 
-
-
 var sum = 2;
 var prime = 3;
 
+//var start = new Date().getTime();
 while (prime < 2000000) {
-  if (isPrime(prime)) {
-    console.log(prime);
-    sum += prime
-  }
-
-  prime = prime+2;
+  if (isPrime(prime)) {sum += prime}
+  prime +=2;
 }
-
-console.log(sum);
+// var end = new Date().getTime();
+//console.log(end - start);
+console.log("SUM: "+sum);
 
 function isPrime(num) {
-  for(var i = 2; i < num; i++)
+  for(var i = 2; i < Math.sqrt(num)+1; i++)
     if(num % i === 0) return false;
   return num !== 1;
 }
